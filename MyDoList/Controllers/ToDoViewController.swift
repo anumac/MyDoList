0559517cde554701ffa1f8820b544ac76a56f9fe
anumac <anu.mac@gmail.com>
2018-10-22 01:42:45 -0500

    check how to pull

--- MyDoList/Controllers/ToDoViewController.swift
+++ MyDoList/Controllers/ToDoViewController.swift
@@ -11,14 +11,36 @@ import UIKit
 
 
 class ToDoViewController: UITableViewController {
-  var itemArray = ["call mondisory", "second", "third"]
+  var itemArray = [item]()
   var defaults = UserDefaults.standard
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "todolist") as? [String]
+        let newItem = item()
+        newItem.title = "first task"
+        let newItem1 = item()
+        newItem1.title = "second Task"
+        
+       itemArray.append(newItem)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        itemArray.append(newItem1)
+        if let items = defaults.array(forKey: "todolist") as? [item]
         {
             itemArray = items
         }
@@ -26,6 +48,7 @@ class ToDoViewController: UITableViewController {
         {
             print("no list")
         }
+ 
     }
     
 
@@ -42,42 +65,59 @@ class ToDoViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        cell.accessoryType =  itemArray[indexPath.row].isDone ? .checkmark : .none
+        
+       
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        print(indexPath.row)
-        print(itemArray[indexPath.row])
+        print(itemArray[indexPath.row].title)
         
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+       
+        
         if tableView.cellForRow(at: indexPath)?.accessoryType != .checkmark
         {
         tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        itemArray[indexPath.row].isDone = true
         }
         else
         {
            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.none
+            itemArray[indexPath.row].isDone = false
         }
+   
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
     
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
         var textField = UITextField()
+        
+        
         let alert = UIAlertController(title: "Add New Do", message: "", preferredStyle: .alert)
         
         let alertAction = UIAlertAction(title: "Add New Do", style: .default) { (alertAction) in
-            if let newItem = textField.text {
+            let newItem = item()
+            if let newTitle = textField.text {
+                newItem.title = newTitle
                 self.itemArray.append(newItem)
+                
                 self.defaults.set(self.itemArray, forKey: "todolist")
                 self.tableView.reloadData()
-        }
-        else
-            {
+              }
+                else
+                {
                 print("No item added")
-            }
+                }
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "add new item"
---
+++ MyDoList/ModelData/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  MyDoList
+//
+//  Created by Anupama Mac on 10/22/18.
+//  Copyright © 2018 Anupama Mac. All rights reserved.
+//
+
+import Foundation
+
+class item {
+    var title : String = ""
+    var isDone : Bool = false
+}
